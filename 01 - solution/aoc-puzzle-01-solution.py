4c2643ba5682996b9ac343b1db9e758bdac8a071
Malcolm Walters <malcolm.walters@ricewarner.com>
2018-12-04 15:33:26 +1100

    Part one - SOLVED

--- 01 - solution/aoc-puzzle-01-solution.py
+++ 01 - solution/aoc-puzzle-01-solution.py
@@ -1,6 +1,20 @@
 #AoC Puzzle 01 Solution
+import os
+from pathlib import Path
 
 startingFrequency = 0
-input_file = 'input-01.txt'
 
-data = open(input_file,'r')
+data_directory = os.path.dirname(os.path.abspath(__file__))
+data_file = 'source-file/input-01.txt'
+
+source_file = os.path.join(data_directory,data_file)
+
+with open(source_file) as f:
+    data = f.readlines()
+    for rows in data:
+        operand = rows[:1]
+        if operand == '+':
+            startingFrequency = startingFrequency + int(rows[1:])
+        else:
+            startingFrequency = startingFrequency - int(rows[1:])
+    print(startingFrequency)

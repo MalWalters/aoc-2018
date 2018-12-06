7b60a8fa01f8ae674bd6abfc6d46e99ac9df1290
Malcolm Walters <malcolm.walters@ricewarner.com>
2018-12-06 14:35:17 +1100

    Correct answer for pt 2.

--- 01 - solution/aoc-puzzle-01-solution.py
+++ 01 - solution/aoc-puzzle-01-solution.py
@@ -2,19 +2,30 @@
 import os
 from pathlib import Path
 
-startingFrequency = 0
-
+frequency = 0
 data_directory = os.path.dirname(os.path.abspath(__file__))
 data_file = 'source-file/input-01.txt'
+frequency_list = []
+change_list = []
+found_duplicate = False
+loop_count = 0
 
 source_file = os.path.join(data_directory,data_file)
 
 with open(source_file) as f:
     data = f.readlines()
+    # Populate change list
     for rows in data:
-        operand = rows[:1]
-        if operand == '+':
-            startingFrequency = startingFrequency + int(rows[1:])
-        else:
-            startingFrequency = startingFrequency - int(rows[1:])
-    print(startingFrequency)
+        change_list.append(rows)
+    while not found_duplicate:
+        loop_count += 1
+        for entries in change_list:
+            if entries[:1] == '+':
+                frequency = frequency + int(entries[1:])
+            else:
+                frequency = frequency - int(entries[1:])
+            if frequency in frequency_list:
+                print("Duplicate Frequency is: "+ str(frequency))
+                found_duplicate = True
+                break
+            frequency_list.append(frequency)

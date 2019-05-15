1f337debb41c94667185e9dedd9271c2f732c354
Malcolm Walters <malcolm.walters@ricewarner.com>
2019-05-15 10:17:30 +1000

    Day 2 part 2 solved

--- 02 - solution/aoc-puzzle-02-solution.py
+++ 02 - solution/aoc-puzzle-02-solution.py
@@ -8,36 +8,17 @@ data_directory = os.path.dirname(os.path.abspath(__file__))
 data_file = 'source-file/aoc-2018-02-input.txt'
 source_file = os.path.join(data_directory,data_file)
 
-def countLetters(rows):
-    unique_list = ''.join(set(rows))
-    return unique_list
-
-def countTwo():
-    
-    return False
-
-def countThree():
-    if countLetters(rows) > 2:
-        return True
-    else:
-        return False
-
 two_count = 0
 three_count = 0
 
 with open(source_file) as f:
-
     data = f.read().splitlines()
-    
     for rows in data:
         counter = Counter(rows)
-        for char in unique_list:
-            if counter[char] > 2:
-                three_count += 1
-                break
-            elif counter[char] > 1:
-                 two_count += 1
-                 break
+        elements = counter.values()
+        if 3 in elements:
+            three_count += 1
+        if 2 in elements:
+            two_count +=1
 
-print(two_count)
-print(three_count)
+print(two_count*three_count)
